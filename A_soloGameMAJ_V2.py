9a317b2761cb84ec12aac32846154396dda72102
Wylohn <wylohn.tribout@gmail.com>
2021-05-16 23:49:30 +0200

    init

--- A_soloGameMAJ_V2.py
+++ A_soloGameMAJ_V2.py
@@ -32,30 +32,6 @@ class SoloGame:
 ###PROPOSITIONS#################################
                 messagerie = Messagerie(self)
 
-                # messagerie.close()
-
-
-                # def Clicked_ResultUser(who):
-
-                #         if who == "bot":
-                #                 print("Click BOT ")
-                #         elif who == "user":
-                #                 print("Click User")
-
-
-                # def Clicked(index,self):
-                #         ##User
-                #         phrase = random_phrase.format(self.mots[index])
-                #         phrase = Phrase_len(phrase)
-                #         repUser = AfficherREP_(self,phrase)
-                #         repUser.clicked.connect(lambda: Clicked_ResultUser("user"))
-
-                #         ##Bot
-                #         phraseBot = random_phrase.format(random.choice(words))
-                #         phraseBot = Phrase_len(phraseBot)
-                #         repBot = AfficherREP_(self,phraseBot)
-                #         repBot.clicked.connect(lambda: Clicked_ResultUser("bot"))
-
                 
                 def AfficherREP (self,phraseReturn):
                         print("RepUSER")
@@ -66,13 +42,18 @@ class SoloGame:
                         self.repUser2.setText(phraseReturn)
                         self.layout_messagerie.addWidget(self.repUser2)
 
-                windowResult = MWindow()
+
                 def Clicked(index,self):
                         print(self.mots[index])
                         print(self.random_phrase.format(self.mots[index]))
                         phrase = self.random_phrase.format(self.mots[index])
                         time.sleep(1.5)
                         AfficherREP (self,phrase)
+
+                        self.proposition1.setVisible(False)
+                        self.proposition2.setVisible(False)
+                        self.proposition3.setVisible(False)
+                        self.proposition4.setVisible(False)
                         
                         
 
@@ -97,47 +78,69 @@ class SoloGame:
                         # self.close()
                         return phraseBot
 
+                def propositions(self,liste):
+                        word = Constructor.test_choose_word(words)
+                        liste.append(word)
+                        proposition1 = Create_button(self,word,440)
+                        word1 = word
+                        proposition1.clicked.connect(lambda: Clicked(0,self))
+                        proposition1.clicked.connect(lambda: score(word1,True))
+                        proposition1.clicked.connect(lambda: Clicked2(self))
+                        proposition1.clicked.connect(lambda: score(word1,False))
 
-                word = Constructor.test_choose_word(words)
-                self.mots.append(word)
-                proposition1 = Create_button(self,word,440)
-                word1 = word
-                proposition1.clicked.connect(lambda: Clicked(0,self))
-                proposition1.clicked.connect(lambda: score(word1,True))
-                proposition1.clicked.connect(lambda: Clicked2(self))
-                proposition1.clicked.connect(lambda: score(word1,False))
+                        word = Constructor.test_choose_word(words)
+                        liste.append(word)
+                        proposition2 = Create_button(self,word,490)
+                        word2 = word
+                        proposition2.clicked.connect(lambda: Clicked(1,self))
+                        proposition2.clicked.connect(lambda: score(word2,True))
+                        proposition2.clicked.connect(lambda: Clicked2(self))
+                        proposition2.clicked.connect(lambda: score(word2,False))
 
-                word = Constructor.test_choose_word(words)
-                self.mots.append(word)
-                proposition2 = Create_button(self,word,490)
-                word2 = word
-                proposition2.clicked.connect(lambda: Clicked(1,self))
-                proposition2.clicked.connect(lambda: score(word2,True))
-                proposition2.clicked.connect(lambda: Clicked2(self))
-                proposition2.clicked.connect(lambda: score(word2,False))
+                        word = Constructor.test_choose_word(words)
+                        liste.append(word)
+                        proposition3 = Create_button(self,word,540)
+                        word3 = word
+                        proposition3.clicked.connect(lambda: Clicked(2,self))
+                        proposition3.clicked.connect(lambda: score(word3,True))
+                        proposition3.clicked.connect(lambda: Clicked2(self))
+                        proposition3.clicked.connect(lambda: score(word3,False))
 
-                word = Constructor.test_choose_word(words)
-                self.mots.append(word)
-                proposition3 = Create_button(self,word,540)
-                word3 = word
-                proposition3.clicked.connect(lambda: Clicked(2,self))
-                proposition3.clicked.connect(lambda: score(word3,True))
-                proposition3.clicked.connect(lambda: Clicked2(self))
-                proposition3.clicked.connect(lambda: score(word3,False))
+                        word = Constructor.test_choose_word(words)
+                        liste.append(word)
+                        proposition4 = Create_button(self,word,590)
+                        word4 = word
+                        proposition4.clicked.connect(lambda: Clicked(3,self))
+                        proposition4.clicked.connect(lambda: score(word4,True))
+                        proposition4.clicked.connect(lambda: Clicked2(self))
+                        proposition4.clicked.connect(lambda: score(word4,False))
 
-                word = Constructor.test_choose_word(words)
-                self.mots.append(word)
-                proposition4 = Create_button(self,word,590)
-                word4 = word
-                proposition4.clicked.connect(lambda: Clicked(3,self))
-                proposition4.clicked.connect(lambda: score(word4,True))
-                proposition4.clicked.connect(lambda: Clicked2(self))
-                proposition4.clicked.connect(lambda: score(word4,False))
+                        proposition4 = Create_button(self,word,590)
+                        proposition4.clicked.connect(lambda: reset())
 
-                print(self.mots)
 
-                motlol = "hello"
-                ScoreBYNassim(motlol)
+                        print(self.mots)
+
+                propositions(self,self.mots)
+
+
+                windowResult = MWindow()
+                def reset():
+                        self.repUser.close()
+                        self.repBot.close()
+                        self.proposition1.setVisible(True)
+                        self.proposition2.setVisible(True)
+                        self.proposition3.setVisible(True)
+                        self.proposition4.setVisible(True)
+
+                        random_phrase2 = Constructor.random_sentence(sentences)
+                        random_phrase2 = Phrase_len(random_phrase2)
+                        PhraseToComplete(self,random_phrase2)
+
+                        liste2 = []
+                        propositions(self,liste2)
+                        
+                        NewGame(self)
 
                 def score(motChoisi, Bool):
                         global resultScore
@@ -229,6 +232,10 @@ class SoloGame:
                                         # VictoirePartie(self)
 
                         return resultScore
+                def NewGame(self):
+                        self.close()
+                        window1 = MWindow()   
+                        SoloGame.__init__(window1)
 
                 # def Victory(gagnerPoint):
                 #         if gagnerPoint :
